2ea55edc5f5fe7652badd81b8b8151174aadc005
Aliaksei Chapyzhenka <alex.drom@gmail.com>
2025-07-04 14:32:25 -0700

    moved some js support functions

--- index.js
+++ index.js
@@ -1,8 +1,17 @@
 'use strict';
 
 const vcd2wasm = require('./vcd2.wasm.js');
+const addWasmMemoryViews = require('./lib/add-wasm-memory-views.js');
+const commandHandler = require('./lib/command-handler.js');
+const initState = require('./lib/init-vcd-worker-state.js');
+const loadWasmNode = require('./lib/load-wasm-node.js');
+const memmap = require('./lib/memmap.js');
 const props = require('./lib/props.js');
 
 exports.vcd2wasm = vcd2wasm;
+exports.addWasmMemoryViews = addWasmMemoryViews;
+exports.commandHandler = commandHandler;
+exports.initState = initState;
+exports.loadWasmNode = loadWasmNode;
+exports.memmap = memmap;
 exports.props = props;
-
